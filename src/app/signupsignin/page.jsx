aa87edde836088ddc50e7d12ddee0982ef141128
chengivan <chengoleivan@gmail.com>
2024-05-12 20:11:35 +0600

    Added github sign in with auth ui

---
+++ src/app/signupsignin/page.jsx
@@ -0,0 +1,16 @@
+import { createClient } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
+
+const App = () => (
+  <Auth
+    supabaseClient={supabase}
+    appearance={{ theme: ThemeSupa }}
+    providers={["github"]}
+  />
+);
